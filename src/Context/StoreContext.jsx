3b485d5c1710b3ad740e3080da0171791e92be95
Rachait Talwar <rachaittalwar159@gmail.com>
2026-06-23 13:00:22 +0530

    new code

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -1,41 +1,56 @@
-import { createContext, useEffect, useState } from "react";
-import { food_list,menu_list } from "../assets/assets";
+import { createContext, useState } from "react";
+import { food_list, menu_list } from "../assets/assets";
+
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const [cartItems,setCartItems] = useState({});
-    const [ordersData,setOrdersData] = useState({});
-    
-    const addToCart = (itemId) =>{
-        if(!cartItems[itemId])
-        {
-            setCartItems((prev)=>({...prev,[itemId]:1}));
-        }
-        else{
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
-        }
-    }
+    const [cartItems, setCartItems] = useState({});
 
-    const removeFromCart = (itemId) =>{
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
-    }
-    
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({
+                ...prev,
+                [itemId]: 1
+            }));
+        } else {
+            setCartItems((prev) => ({
+                ...prev,
+                [itemId]: prev[itemId] + 1
+            }));
+        }
+    };
+
+    const removeFromCart = (itemId) => {
+        if (cartItems[itemId] > 1) {
+            setCartItems((prev) => ({
+                ...prev,
+                [itemId]: prev[itemId] - 1
+            }));
+        } else {
+            setCartItems((prev) => {
+                const newCart = { ...prev };
+                delete newCart[itemId];
+                return newCart;
+            });
+        }
+    };
+
     const getTotalCartAmount = () => {
         let totalAmount = 0;
+
         for (const item in cartItems) {
-          if (cartItems[item] > 0) {
-            let itemInfo = food_list.find((product) => product.food_id === Number(item));
-            totalAmount += itemInfo.food_price * cartItems[item];
-          }
+            const itemInfo = food_list.find(
+                (product) => product.food_id === Number(item)
+            );
+
+            if (itemInfo) {
+                totalAmount += itemInfo.food_price * cartItems[item];
+            }
         }
+
         return totalAmount;
-      }
-
-    const placeOrder = (deliveryData) =>{
-
-        console.log(deliveryData);
-    }
+    };
 
     const contextValue = {
         food_list,
@@ -43,16 +58,14 @@ const StoreContextProvider = (props) => {
         cartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount,
-        placeOrder
+        getTotalCartAmount
     };
 
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
-        )
-
-}
+    );
+};
 
 export default StoreContextProvider;
--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,24 +1,45 @@
-import React, { useContext } from 'react'
-import './FoodDisplay.css'
-import FoodItem from '../FoodItem/FoodItem'
-import { StoreContext } from '../../Context/StoreContext'
+import React, { useContext } from 'react';
+import './FoodDisplay.css';
+import FoodItem from '../FoodItem/FoodItem';
+import { StoreContext } from '../../Context/StoreContext';
 
-const FoodDisplay = ({category}) => {
+const FoodDisplay = ({ category }) => {
 
-  const {food_list} = useContext(StoreContext);
+    const { food_list } = useContext(StoreContext);
 
-  return (
-    <div className='food-display' id='food-display'>
-      <h2>Top dishes near you </h2>
-      <div className='food-display-list'>
-        {food_list.map((item)=>{
-          if (category==="All" || category===item.food_category) {
-            return <FoodItem key={item.food_id} image={item.food_image} name={item.food_name} desc={item.food_desc} price={item.food_price} id={item.food_id}/>
-          }
-        })}
-      </div>
-    </div>
-  )
-}
+    console.log("Food List:", food_list);
+    console.log("Selected Category:", category);
 
-export default FoodDisplay
+    return (
+        <div className='food-display' id='food-display'>
+            <h2>Top dishes near you</h2>
+
+            <div className='food-display-list'>
+
+                {food_list &&
+                    food_list.map((item) => {
+
+                        if (
+                            category === "All" ||
+                            category === item.food_category
+                        ) {
+                            return (
+                                <FoodItem
+                                    key={item.food_id}
+                                    id={item.food_id}
+                                    image={item.food_image}
+                                    name={item.food_name}
+                                    desc={item.food_desc}
+                                    price={item.food_price}
+                                />
+                            );
+                        }
+
+                        return null;
+                    })}
+            </div>
+        </div>
+    );
+};
+
+export default FoodDisplay;
